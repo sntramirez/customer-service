ab2b360300e034b649b9818fb9b1efdf265cdaca
Santiago Ramirez <snt.ramirez@gmail.com>
2024-04-28 23:01:54 -0500

    test integracion persona

--- src/main/java/com/devsu/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/devsu/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,23 @@
 package com.devsu.customerservice;
 
+import com.devsu.customerservice.infraestructure.data.entities.Persona;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication( scanBasePackages = {"com.devsu"})
-public class CustomerServiceApplication {
+public class CustomerServiceApplication implements RepositoryRestConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(final RepositoryRestConfiguration config,
+                                                     final CorsRegistry cors) {
+        config.exposeIdsFor(Persona.class);
     }
 
 }
